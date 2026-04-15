38eb0757470d7e174ead7332f9062a2e424071b1
GaskaPiotr <04piotr04@wp.pl>
2026-04-15 08:17:17 +0200

    fet: add login function and http client
    Adds login function that logs value
    Also add http client for api requests

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  http = inject(HttpClient);
+
+  email = '';
+  password = '';
+  rPassword = '';
+
+  sendLogin() {
+    console.log("Try to login with:");
+  }
+}
