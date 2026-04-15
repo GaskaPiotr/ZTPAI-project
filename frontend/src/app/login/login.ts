ad6040e9a93fdba91fb445e0d48163d491acd474
GaskaPiotr <04piotr04@wp.pl>
2026-04-15 08:58:25 +0200

    fet: add webconfig to allow 4200 port
    Adds webconfig to allow 4200 get, post, put and delete
    Also adds the correct login api path

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -29,7 +29,7 @@ export class Login {
 
     console.log("Try to authenticate");
 
-    this.http.post('http://localhost:8080/api/login', sendData)
+    this.http.post('http://localhost:8080/api/v1/auth/login', sendData)
       .subscribe({
         next: (answer) => {
           console.log("Login successful")
