f66dc70c3be6aa6b31334be021e58902918d4ccd
GaskaPiotr <04piotr04@wp.pl>
2026-05-04 11:52:58 +0200

    fet: add register

--- frontend/src/app/register/register.ts
+++ frontend/src/app/register/register.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+  http = inject(HttpClient);
+
+  email = '';
+  password = '';
+  rPassword = '';
+
+  sendRegister() {
+    if (this.password != this.rPassword) {
+      console.log("Different password");
+      alert("Passwords must be the same!")
+      return;
+    }
+
+    const sendData = {
+      email: this.email,
+      password: this.password
+    };
+
+    console.log("Try to authenticate");
+
+    this.http.post('http://localhost:8080/api/v1/auth/register', sendData)
+      .subscribe({
+        next: (answer) => {
+          console.log("Register successful")
+        },
+        error: (e) => {
+          console.error("Authentication error: ", e);
+        }
+      })
+
+  }
+}
