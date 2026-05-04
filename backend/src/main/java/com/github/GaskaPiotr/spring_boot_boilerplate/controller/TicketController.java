1fba14324d9b7cf2f8db832742c967c24b7b6ff4
GaskaPiotr <04piotr04@wp.pl>
2026-05-04 19:36:30 +0200

    fet: add ticket controller endpoints

--- backend/src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/controller/TicketController.java
+++ backend/src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/controller/TicketController.java
@@ -1,22 +1,41 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.controller;
 
+import com.github.GaskaPiotr.spring_boot_boilerplate.dto.TicketRequest;
+import com.github.GaskaPiotr.spring_boot_boilerplate.dto.TicketResponse;
+import com.github.GaskaPiotr.spring_boot_boilerplate.service.TicketService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/tickets")
+@RequiredArgsConstructor
 public class TicketController {
-    @PostMapping
-    public void addTicket() {
+    private final TicketService ticketService;
 
+    @PostMapping
+    public ResponseEntity<TicketResponse> addTicket(
+            @Valid @RequestBody TicketRequest request,
+            Principal principal
+            ) {
+        TicketResponse response = ticketService.addTicket(request, principal.getName());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public List<> getTickets() {
-
+    public ResponseEntity<List<TicketResponse>> getTickets(Principal principal) {
+        List<TicketResponse> tickets = ticketService.getTickets(principal.getName());
+        return ResponseEntity.ok(tickets);
     }
 
     @PatchMapping("{id}/resolve")
-    public void changeTicketStatus() {
-
+    public ResponseEntity<Void> changeTicketStatus(@PathVariable Long id) {
+        ticketService.resolveTicket(id);
+        return ResponseEntity.ok().build();
     }
 }
